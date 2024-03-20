4a1ede257d29cdec714f466456fd9663f96b7a31
Christien Alden <christien.alden@zoho.com>
2024-03-20 21:16:06 +0800

    working but needs work

--- gui-src/gui.cpp
+++ gui-src/gui.cpp
@@ -5,6 +5,57 @@
 #include <backends/imgui_impl_opengl2.h>
 #include <stdio.h>
 
+bool rotateAxis(Gimbal* gimbal, Axis axis, char* label, float speed)
+{
+	bool active = false;
+	ImGui::PushID(label);
+	ImGui::Text(label);
+	ImGui::SameLine();
+	ImGui::PushItemWidth(100.0f);
+	char id[4];
+	snprintf(id, sizeof(id), "##%c", *label);
+	float angle = gimbal->rotation[axis];
+	if (ImGui::DragFloat(id, &angle, 1.0f, 0.0f, 0.0f) || ImGui::IsItemActive())
+	{
+		active = true;
+		if (angle >= 360.0f)
+		{
+			angle -= 360.0f;
+		}
+		else if (angle < -360.0f)
+		{
+			angle += 360.0f;
+
+		}
+
+		gimbal->rotation[axis] = angle;
+	}
+
+	ImGui::PopItemWidth();
+	ImGui::SameLine();
+	if (ImGui::Button("+") || ImGui::IsItemActive())
+	{
+		active = true;
+		gimbal->rotation[axis] += speed * ImGui::GetIO().DeltaTime;
+		if (gimbal->rotation[axis] >= 360.0f)
+		{
+			gimbal->rotation[axis] -= 360.0f;
+		}
+	}
+	ImGui::SameLine();
+	if (ImGui::Button("-") || ImGui::IsItemActive())
+	{
+		active = true;
+		gimbal->rotation[axis] -= speed * ImGui::GetIO().DeltaTime;
+		if (gimbal->rotation[axis] < -360.0f)
+		{
+			gimbal->rotation[axis] += 360.0f;
+		}
+	}
+	ImGui::PopID();
+	return active;
+}
+
 void gui_init()
 {
 	IMGUI_CHECKVERSION();
@@ -14,6 +65,13 @@ void gui_init()
 	// can't remove this otherwise the program crashes at launch
 	ImGuiIO &io = ImGui::GetIO(); (void)io;
 	io.DisplaySize = ImVec2(500, 500);
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.WindowRounding = 4.0f;
+	style.FrameRounding = 4.0f;
+	style.GrabRounding = 2.0f;
+	style.Alpha = 1.0f;
+	style.WindowMenuButtonPosition = ImGuiDir_None;
 
 	ImGui_ImplGLUT_Init();
 	ImGui_ImplOpenGL2_Init();
@@ -28,64 +86,69 @@ void gui_update(Gimbal* gimbal)
 	ImGui_ImplGLUT_NewFrame();
 	ImGui::NewFrame();
 
+	ImGui::SetNextWindowSize(ImVec2(200, 300));
 	ImGui::Begin("Euler Rotation Demo");
-	ImGui::Checkbox("Toggle Axes", &gimbal->drawAxes);
-	ImGui::Checkbox("Toggle Rotations", &gimbal->drawRotations);
-	const char* items[] = { "XYZ", "XZY", "YXZ", "YZX", "ZXY", "ZYX" };
-	static int currentItem = 0;
-	static float degPerSecond = 10.0f;
-	enum Axis activeAxis = AXIS_NONE;
-	if (ImGui::Combo("Mode", &currentItem, items, IM_ARRAYSIZE(items)))
-	{
-		gimbal->eulerMode = (enum EulerMode) currentItem;
-	}
-	ImGui::SliderFloat("Speed", &degPerSecond, 1.0f, 60.0f);
-	for (int i = 0; i < 3; i++)
-	{
-		static char category[10];
-		snprintf(category, 20, "rot_cat_%c", "XYZ"[i]);
-		ImGui::PushID(category);
-		static char label[3];
-		snprintf(label, 3, "##%c", "XYZ"[i]);
-		ImGui::Text("%c", "XYZ"[i]);
+		ImGui::SeparatorText("Display Options");
+		ImGui::Checkbox("Axes", &gimbal->drawAxes);
+		ImGui::SameLine(0.0f, 10.0f);
+		ImGui::Checkbox("Gizmo", &gimbal->drawRotations);
+		ImGui::Spacing();
+
+		// euler mode - first row
+		ImGui::SeparatorText("Euler Mode");
+		ImGui::RadioButton("XYZ", (int*) &gimbal->eulerMode, EULER_MODE_XYZ);
+		ImGui::SameLine(0.0f, 10.0f);
+		ImGui::RadioButton("YXZ", (int*) &gimbal->eulerMode, EULER_MODE_YXZ);
+		ImGui::SameLine(0.0f, 10.0f);
+		ImGui::RadioButton("ZXY", (int*) &gimbal->eulerMode, EULER_MODE_ZXY);
+		// euler mode - second row
+		ImGui::RadioButton("XZY", (int*) &gimbal->eulerMode, EULER_MODE_XZY);
+		ImGui::SameLine(0.0f, 10.0f);
+		ImGui::RadioButton("YZX", (int*) &gimbal->eulerMode, EULER_MODE_YZX);
+		ImGui::SameLine(0.0f, 10.0f);
+		ImGui::RadioButton("ZYX", (int*) &gimbal->eulerMode, EULER_MODE_ZYX);
+		ImGui::Spacing();
+
+		static float rotationDegPerSecond = 10.0f;
+		enum Axis activeAxis = AXIS_NONE;
+		ImGui::SeparatorText("Rotations");
+		ImGui::Text(" ");
 		ImGui::SameLine();
-		ImGui::PushItemWidth(50.0f);
-		ImGui::InputFloat(label, &gimbal->rotation[i], 0.0f, 0.0f, "%.2f", ImGuiInputTextFlags_ReadOnly);
-		ImGui::PopItemWidth();
-		ImGui::SameLine();
-		if (ImGui::Button("Positive") || ImGui::IsItemActive())
+		ImGui::PushItemWidth(100.0f);
+		if (ImGui::DragFloat(" deg/s", &rotationDegPerSecond, 1.0f, 60.0f))
 		{
-			activeAxis = (enum Axis) i;
-			gimbal->rotation[i] += degPerSecond * ImGui::GetIO().DeltaTime;
-			if (gimbal->rotation[i] >= 360.0f)
+			if (rotationDegPerSecond < 0.0f)
 			{
-				gimbal->rotation[i] -= 360.0f;
+				rotationDegPerSecond = 0.0f;
+			}
+			else if (rotationDegPerSecond > 360.0f)
+			{
+				rotationDegPerSecond = 360.0f;
 			}
 		}
-		ImGui::SameLine();
-		if (ImGui::Button("Negative") || ImGui::IsItemActive())
+		if (rotateAxis(gimbal, AXIS_X, "X", rotationDegPerSecond))
 		{
-			activeAxis = (enum Axis) i;
-			gimbal->rotation[i] -= degPerSecond * ImGui::GetIO().DeltaTime;
-			if (gimbal->rotation[i] < -360.0f)
-			{
-				gimbal->rotation[i] += 360.0f;
-			}
+			activeAxis = AXIS_X;
 		}
-		ImGui::PopID();
-	}
-	if (ImGui::Button("Reset Rotations"))
-	{
-		gimbal->rotation[0] = 0.0f;
-		gimbal->rotation[1] = 0.0f;
-		gimbal->rotation[2] = 0.0f;
-	}
-	if (ImGui::Button("Quit"))
-	{
-		glutLeaveMainLoop();
-	}
+		if (rotateAxis(gimbal, AXIS_Y, "Y", rotationDegPerSecond))
+		{
+			activeAxis = AXIS_Y;
+		}
+		if (rotateAxis(gimbal, AXIS_Z, "Z", rotationDegPerSecond))
+		{
+			activeAxis = AXIS_Z;
+		}
+		gimbal->activeAxis = activeAxis;
+		ImGui::Spacing();
+
+		ImGui::Separator();
+		if (ImGui::Button("Reset Orientation"))
+		{
+			gimbal->rotation[0] = 0.0f;
+			gimbal->rotation[1] = 0.0f;
+			gimbal->rotation[2] = 0.0f;
+		}
 	ImGui::End();
-	gimbal->activeAxis = activeAxis;
 }
 
 void gui_render()
